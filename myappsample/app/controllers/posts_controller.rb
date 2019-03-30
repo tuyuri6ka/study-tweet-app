672a6e938450f30dd86ef009493dcdc6ca7cfc84
tuyuri6ka <47937296+tuyuri6ka@users.noreply.github.com>
2019-03-30 15:20:50 +0900

    投稿成功・削除成功のflash変数を追加しました。

--- myappsample/app/controllers/posts_controller.rb
+++ myappsample/app/controllers/posts_controller.rb
@@ -14,6 +14,7 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(content: params[:content])
     if @post.save
+      flash[:notice]="投稿を作成しました"
       redirect_to("/posts/index")
     else
       render("posts/new")
@@ -36,6 +37,7 @@ class PostsController < ApplicationController
   end
   
   def destroy
+    flash[:notice]="投稿を削除しました"
     @post = Post.find_by(id: params[:id])
     @post.destroy
     redirect_to("/posts/index")
