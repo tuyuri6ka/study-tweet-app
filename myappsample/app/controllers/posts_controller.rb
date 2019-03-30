ae822381ff7d8ffcb2b1b02ba8d79687769b5c43
tuyuri6ka <47937296+tuyuri6ka@users.noreply.github.com>
2019-03-30 11:59:44 +0900

    編集update機能の追加

--- myappsample/app/controllers/posts_controller.rb
+++ myappsample/app/controllers/posts_controller.rb
@@ -11,8 +11,18 @@ class PostsController < ApplicationController
   end
   
   def create
-    # フォームから送信されたデータを受け取り、保存する処理を追加してください
     @post = Post.new(content: params[:content])
+    @post.save
+    redirect_to("/posts/index")
+  end
+
+  def edit
+    @post = Post.find_by(id:params[:id])
+  end
+
+  def update
+    @post = Post.find_by(id: params[:id])
+    @post.content = params[:content]
     @post.save
     redirect_to("/posts/index")
   end
--- myappsample/config/routes.rb
+++ myappsample/config/routes.rb
@@ -8,6 +8,10 @@ Rails.application.routes.draw do
   get "/posts/index" => "posts#index"
   get "/posts/new" => "posts#new"
   post "/posts/create" => "posts#create"
+  get "/posts/:id/edit" =>"posts#edit"
+  post "/posts/:id/update" =>"posts#update"
+
   get "/posts/:id" => "posts#show"  ##show.html.erbに:idを渡す。このgetは必ず後ろに。
+
 
 end
