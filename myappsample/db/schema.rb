13999d6018a90efc4ce7275c3e731e08f41615b4
tuyuri6ka <47937296+tuyuri6ka@users.noreply.github.com>
2019-03-31 11:10:06 +0900

    add_image_name_to_usersマイグレーションを作成し、add_columnした。

--- myappsample/db/schema.rb
+++ myappsample/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_30_065145) do
+ActiveRecord::Schema.define(version: 2019_03_31_015230) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
