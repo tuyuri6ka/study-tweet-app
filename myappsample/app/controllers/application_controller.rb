abe5805cfac791fb051881c42cc89c35f507a48a
tuyuri6ka <47937296+tuyuri6ka@users.noreply.github.com>
2019-04-01 00:17:07 +0900

    before_actionメソッドを学習。current_userを定義し、apphtml側を変更。

--- myappsample/app/controllers/application_controller.rb
+++ myappsample/app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+
+    #すべてのcontrollerに対するテンプレートみたいなもの
+    before_action :set_current_user
+
+    #現在のユーザー情報をcurrent_user変数に保存
+    def set_current_user
+        @current_user = User.find_by(id: session[:user_id])
+    end
+
 end
