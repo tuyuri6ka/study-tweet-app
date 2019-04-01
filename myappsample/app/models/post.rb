a179bc97864fa9860996f231b0a5402c97ee13b2
tuyuri6ka <47937296+tuyuri6ka@users.noreply.github.com>
2019-04-02 00:34:39 +0900

    postsテーブルにuser_idカラム追加のため、migrationファイル作成とchange修正

--- myappsample/app/models/post.rb
+++ myappsample/app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
     validates(:content, {presence: true}) #validatesメソッドの()はなくてもよい。その場合:contentとの間に空白を設ける
     validates(:content, {length:{maximum:140}})
+    validates(:user_id, {presence: true})
 end
