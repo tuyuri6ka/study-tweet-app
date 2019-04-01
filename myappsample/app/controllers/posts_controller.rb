e09b1d8f11a7d7d30d0306aa177eb1702cb0ac3e
tuyuri6ka <47937296+tuyuri6ka@users.noreply.github.com>
2019-04-02 02:35:48 +0900

    post詳細（show.html）にユーザー画像を表示できるようにshowアクションにも機能追加。

--- myappsample/app/controllers/posts_controller.rb
+++ myappsample/app/controllers/posts_controller.rb
@@ -8,6 +8,7 @@ class PostsController < ApplicationController
   
   def show
     @post = Post.find_by(id: params[:id])
+    @user = User.find_by(id: @post.user_id) #ポスト情報にあるuser_idを取得
   end
   
   def new
