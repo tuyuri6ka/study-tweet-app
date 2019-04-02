37184dce0490b8778591b64d2e992ee2e3ee68ac
tuyuri6ka <47937296+tuyuri6ka@users.noreply.github.com>
2019-04-02 18:57:16 +0900

    before_actionでensure_correct...を利用し、編集へのアクセス制限を追加。

--- myappsample/app/controllers/posts_controller.rb
+++ myappsample/app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
 
   before_action :authenticate_user
+  before_action :ensure_correct_user,{only: [:edit, :update, :destroy]}
 
   def index
     @posts = Post.all.order(created_at: :desc)
@@ -52,4 +53,12 @@ class PostsController < ApplicationController
     redirect_to("/posts/index")
   end
   
+  def ensure_correct_user
+    @post = Post.find_by(id: params[:id])
+    if @post.user_id != @current_user.id
+      flash[:notice]="権限がありません"
+      redirect_to("/posts/index")
+    end
+  end
+
 end
